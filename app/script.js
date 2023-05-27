23db6343b1aab21efb945fb66f3f00cccf0b4b3f
Felipe Antoniati <antoniati.felipe@gmail.com>
2023-05-27 14:00:33 -0300

    creating menu-toggle and dropdown-menu

--- app/script.js
+++ app/script.js
@@ -1,2 +1,12 @@
-const helloElement = document.querySelector("h1")
-console.log(helloElement)
+var MenuToggle = {
+    open: function() {
+      var hamburger = document.querySelector('.menu-toggle');
+      hamburger.classList.toggle('open');
+      var navContent = document.querySelector(".nav-content")
+      navContent.classList.toggle("open")
+      var contentRight = document.querySelector(".nav-content-right")
+      contentRight.classList.toggle("open")
+      var menuList = document.querySelector(".menu-list")
+      menuList.classList.toggle("open")
+    }
+  };
