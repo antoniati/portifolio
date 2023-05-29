ae4b696d1a18de3a52c46ad62b62a13d8d54b980
Felipe Antoniati <antoniati.felipe@gmail.com>
2023-05-29 11:57:47 -0300

    presentation session creation, css modification and responsiveness

--- app/script.js
+++ app/script.js
@@ -10,3 +10,24 @@ var MenuToggle = {
       menuList.classList.toggle("open")
     }
   };
+
+  // JavaScript to handle modal functionality
+  document.addEventListener("DOMContentLoaded", function() {
+    var modal = document.getElementById("myModal");
+    var btn = document.getElementById("openModal");
+    var span = document.getElementsByClassName("close")[0];
+
+    btn.onclick = function() {
+      modal.style.display = "block";
+    }
+
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+  });
