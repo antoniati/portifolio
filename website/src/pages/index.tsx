f1723531fc8c7438cd1068c691eef1c64d3187bd
Felipe Antoniati <antoniati.felipe@gmail.com>
2023-07-29 20:09:57 -0300

    add: Next.js in project / Hello World

---
+++ website/src/pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Programador Freelancer | Felipe Antoniati - Websites Modernos e Personalizados</title>
+        <meta name="description" content="Programdor freelancer especializado em criar websites modernos e personalizados. Trabalho com clientes de diversos setores para fornecer soluções digitais de alta qualidade. Entre em contato e vamos transformar sua visão em realidade" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1>Hello World</h1>
+      </main>
+    </>
+  )
+}
