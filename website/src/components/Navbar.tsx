84f57be2ec3e76e6c2988f94b734aa78a93f73db
Felipe Antoniati <antoniati.felipe@gmail.com>
2023-07-30 10:13:50 -0300

    Navbar: add styles

--- website/src/components/Navbar.tsx
+++ website/src/components/Navbar.tsx
@@ -2,27 +2,30 @@ import { constantsData } from "@/data/constantsData";
 import styles from "@/styles/Navbar.module.css";
 
 const Navbar = () => {
-
-    const { logo, pageLinks, navItemTexts} = constantsData;
+    const { logo, pageLinks, navItemTexts } = constantsData;
 
     return (
-        <nav className={styles.navbarContainer}>
+        <nav id={styles.navbarContainer}>
             <div className={styles.navbarContent}>
                 <div className={styles.navbarLogo}>
                     <img className={styles.navbarLogoImage} src="/logo.svg" alt={logo.logoAltText} />
-                    <h1 className={styles.navbarLogoText}> {logo.logoText} </h1>
+                    <h1 className={styles.navbarLogoText}> 
+                        {logo.logoText} 
+                        <span>Programador Freelancer</span>
+                    </h1>
+                    
                 </div>
                 <ul className={styles.navList}>
-                    <a className={styles.navItem} href={pageLinks.URLToAboutPage}>
+                    <a className={`${styles.navItem} ${styles.navItemWrapper}`} href={pageLinks.URLToAboutPage}>
                         {navItemTexts.about}
                     </a>
-                    <a className={styles.navItem} href={pageLinks.URLToPortifolioPage}>
+                    <a className={`${styles.navItem} ${styles.navItemWrapper}`} href={pageLinks.URLToPortifolioPage}>
                         {navItemTexts.portifolio}
                     </a>
-                    <a className={styles.navItem} href={pageLinks.URLToServicesPage}>
+                    <a className={`${styles.navItem} ${styles.navItemWrapper}`} href={pageLinks.URLToServicesPage}>
                         {navItemTexts.services}
                     </a>
-                    <a className={styles.navItem} href={pageLinks.URLToContactPage}>
+                    <a className={`${styles.navItem} ${styles.navItemWrapper}`} href={pageLinks.URLToContactPage}>
                         {navItemTexts.contact}
                     </a>
                 </ul>
