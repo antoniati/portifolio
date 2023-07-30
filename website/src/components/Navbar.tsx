9e8914cfa1712a9c96743f1a5c8dd60e794455f0
Felipe Antoniati <antoniati.felipe@gmail.com>
2023-07-30 11:41:27 -0300

    Navbar: creating and add Drop Down Menu

--- website/src/components/Navbar.tsx
+++ website/src/components/Navbar.tsx
@@ -1,38 +1,62 @@
-import { constantsData } from "@/data/constantsData";
-import styles from "@/styles/Navbar.module.css";
-import ToggleMenu from "./ToggleMenu";
+import { useState } from 'react';
+import ToggleMenu from '@/components/ToggleMenu';
+import DropdownMenu from '@/components/DropDownMenu';
+import { constantsData } from '@/data/constantsData';
+import styles from '@/styles/Navbar.module.css';
 
 const Navbar = () => {
-    const { logo, pageLinks, navItemTexts } = constantsData;
+  const { logo, pageLinks, navItemTexts } = constantsData;
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-    return (
-        <nav id={styles.navbarContainer}>
-            <div className={styles.navbarContent}>
-                <div className={styles.navbarLogo}>
-                    <img className={styles.navbarLogoImage} src="/logo.svg" alt={logo.logoAltText} />
-                    <h1 className={styles.navbarLogoText}> 
-                        {logo.logoText} 
-                        <span>Programador Freelancer</span>
-                    </h1>
-                </div>
-                <ul className={styles.navList}>
-                    <a className={`${styles.navItem} ${styles.navItemWrapper}`} href={pageLinks.URLToAboutPage}>
-                        {navItemTexts.about}
-                    </a>
-                    <a className={`${styles.navItem} ${styles.navItemWrapper}`} href={pageLinks.URLToPortifolioPage}>
-                        {navItemTexts.portifolio}
-                    </a>
-                    <a className={`${styles.navItem} ${styles.navItemWrapper}`} href={pageLinks.URLToServicesPage}>
-                        {navItemTexts.services}
-                    </a>
-                    <a className={`${styles.navItem} ${styles.navItemWrapper}`} href={pageLinks.URLToContactPage}>
-                        {navItemTexts.contact}
-                    </a>
-                </ul>
-                <ToggleMenu />
-            </div>
-        </nav>
-    );
+  const toggleMenuHandler = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <nav id={styles.navbarContainer}>
+      <div className={styles.navbarContent}>
+        <div className={styles.navbarLogo}>
+          <img
+            className={styles.navbarLogoImage}
+            src="/logo.svg"
+            alt={logo.logoAltText}
+          />
+          <h1 className={styles.navbarLogoText}>
+            {logo.logoText}
+            <span>Programador Freelancer</span>
+          </h1>
+        </div>
+        <ul className={styles.navList}>
+          <a
+            className={`${styles.navItem} ${styles.navItemWrapper}`}
+            href={pageLinks.URLToAboutPage}
+          >
+            {navItemTexts.about}
+          </a>
+          <a
+            className={`${styles.navItem} ${styles.navItemWrapper}`}
+            href={pageLinks.URLToPortifolioPage}
+          >
+            {navItemTexts.portifolio}
+          </a>
+          <a
+            className={`${styles.navItem} ${styles.navItemWrapper}`}
+            href={pageLinks.URLToServicesPage}
+          >
+            {navItemTexts.services}
+          </a>
+          <a
+            className={`${styles.navItem} ${styles.navItemWrapper}`}
+            href={pageLinks.URLToContactPage}
+          >
+            {navItemTexts.contact}
+          </a>
+        </ul>
+        <ToggleMenu onClick={toggleMenuHandler} isOpen={isMenuOpen} />
+        {isMenuOpen && <DropdownMenu isOpen={isMenuOpen} />}
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
--- website/src/components/ToggleMenu.tsx
+++ website/src/components/ToggleMenu.tsx
@@ -1,23 +1,22 @@
-import { useState } from 'react';
+import React from 'react';
 import styles from '@/styles/ToggleMenu.module.css';
 
-const ToggleMenu = () => {
-    const [isOpen, setIsOpen] = useState(false);
+interface ToggleMenuProps {
+  isOpen: boolean;
+  onClick: () => void;
+}
 
-    const toggleMenuHandler = () => {
-        setIsOpen(!isOpen);
-    };
-
-    return (
-        <span
-            className={`${styles.toggleMenuButton} ${isOpen ? styles.open : ''}`}
-            onClick={toggleMenuHandler}
-        >
-            <span></span>
-            <span></span>
-            <span></span>
-        </span>
-    );
+const ToggleMenu: React.FC<ToggleMenuProps> = ({ isOpen, onClick }) => {
+  return (
+    <button
+      className={`${styles.toggleMenuButton} ${isOpen ? styles.open : ''}`}
+      onClick={onClick}
+    >
+      <span></span>
+      <span></span>
+      <span></span>
+    </button>
+  );
 };
 
 export default ToggleMenu;
