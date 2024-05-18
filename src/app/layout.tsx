68078b4bfc17f1e311691e23acae81e6cf18da36
antoniati <antoniati.felipe@gmail.com>
2024-05-18 11:25:34 -0300

    under construction

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Desenvolvedor Full Stack | Antoniati",
+  description: "Especialista em Next.js, React.js, JavaScript e TypeScript.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
